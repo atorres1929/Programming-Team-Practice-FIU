3c87549c568dc5765c7a81715d256ade6a0db6a2
Adam Torres <atorres1929@gmail.com>
2018-02-22 21:53:31 -0500

    A Classy Problem Testing

--- Python/ClassyProblem/ClassyProblem.py
+++ Python/ClassyProblem/ClassyProblem.py
@@ -1,26 +1,5 @@
-names = []
+names = {}
 maxClassLength = 0
-
-
-def checkClass(namesList):
-    output = {}
-    for n in namesList:
-        for name, classes in n.items():
-
-            output[name] = 0
-
-            while len(classes) < maxClassLength:
-                classes.append("middle")
-
-            for clazz in classes:
-                if clazz == "upper":
-                    output[name] += 3
-                elif clazz == "middle":
-                    output[name] += 2
-                else:
-                    output[name] += 1
-    for item in sorted(output.items(), key=lambda x: (-x[1], x[0])):
-        print(item[0])
 
 
 testCases = int(input())
@@ -29,17 +8,20 @@ while testCases > 0:
     while numNames > 0:
         line = input()
         line = line.split()
-
         name = line[0][:-1]
         classes = line[1].split('-')
+        names[name] = 0
 
-        names.append(
-            {name: classes}
-        )
-
-        if maxClassLength < len(classes):
-            maxClassLength = len(classes)
+        for clazz in classes:
+            if clazz == "upper":
+                names[name] += 1
+            elif clazz == "lower":
+                names[name] -= 1
         numNames -= 1
+
+    for item in sorted(names.items(), key=lambda x: (-x[1], x[0])):
+        print(item[0])
+
     testCases -= 1
-    checkClass(names)
+    names.clear()
     print("==============================")
