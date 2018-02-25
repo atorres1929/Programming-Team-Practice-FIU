185ab30248e29f8cac0ae28df20c925358bd6f9b
Adam Torres <atorres1929@gmail.com>
2018-02-25 18:34:49 -0500

    Classy Problem Complete

--- Python/ClassyProblem/ClassyProblem.py
+++ Python/ClassyProblem/ClassyProblem.py
@@ -2,6 +2,29 @@ names = {}
 maxClassLength = 0
 
 
+def checkClass(namesList):
+    output = {}
+    for n in namesList:
+        for name, classes in n.items():
+
+            output[name] = 0
+            classes = list(reversed(classes))
+            while len(classes) < maxClassLength:
+                classes.append("middle")
+
+            for clazz in classes:
+                if clazz == "upper":
+                    output[name] += 3
+                elif clazz == "middle":
+                    output[name] += 2
+                else:
+                    output[name] += 1
+                output[name] *= 10
+
+    for item in sorted(output.items(), key=lambda x: (-x[1], x[0])):
+        print(item[0])
+
+
 testCases = int(input())
 while testCases > 0:
     numNames = int(input())
@@ -23,5 +46,6 @@ while testCases > 0:
         print(item[0])
 
     testCases -= 1
+    checkClass(names)
     names.clear()
     print("==============================")
